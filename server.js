59eb877956efce311fe658bfa6867cec86f47779
mtugrulavci <mustafa.tugrulavci@alcumus.com>
2021-06-23 21:04:11 -0400

    added param

--- server.js
+++ server.js
@@ -45,6 +45,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
 
 app.get('/api/animals', (req, res) => {
     let results = animals; //assigns animal data to results that imported from animals.js
@@ -53,6 +58,15 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
   });
+
+app.get('/api/animals/:id', (req,res)=>{
+    const result =findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+      } else {
+        res.send(404);
+      }
+})
 
 
 app.listen(PORT,()=>{
